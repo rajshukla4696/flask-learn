cf681f49bac434f996887ef8dbc5c9f53b89aae2
rajshukla <rajshukla4696@gmail.com>
2024-07-13 09:51:51 +0530

    Tests Added

---
+++ test_app.py
@@ -0,0 +1,31 @@
+import pytest
+from app import app
+import json
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_home(client):
+    resp = client.get("/ping")
+    assert resp.status_code == 404
+
+
+def test_root(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
+
+
+def test_prediction(client):
+    test_data = {
+        "Gender": "Male",
+        "Married": "Unmarried",
+        "ApplicantIncome": 50000,
+        "Credit_History": "Cleared Debts",
+        "LoanAmount": 500000
+    }
+    resp = client.post("/prediction", json=test_data)
+    assert resp.status_code == 200
+    assert resp.json == {"loan_approval_status": "Rejected"}
